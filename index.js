376451a4f84e692f41ec7d5ca46c2a8b5be6cbfc
ellis <lsp1@hotmail.com>
2022-09-06 08:45:40 -0600

    select random breeds image & description

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* PAGE ELEMENTS */
+/* QUERY PAGE ELEMENTS */
 
 // get array of buttons on the page
 const buttons = document.querySelectorAll("button");
@@ -6,7 +6,10 @@ const buttons = document.querySelectorAll("button");
 // get the image element used to display the pages image and random images
 const imageHolder = document.querySelector('img[alt="No kittie for you!"]');
 
-/*  GET RANDOM FACT SCRIPTS */
+/*  GET AND DISPLAY RANDOM FACT SCRIPTS */
+
+// display fact on initial page visit
+getCatFact();
 
 // get button when pressed will fetch and display a new cat fact
 const getFactButton = buttons[1];
@@ -30,35 +33,69 @@ async function getCatFact() {
   displayCatFact(fact);
 }
 
-// display fact on initial page visit
-getCatFact();
+/* GET AND DISPLAY A RANDOM BREED'S IMAGE AND DESCRIPTION SCRIPTS*/
 
-/* GET RANDOM IMAGE SCRIPTS */
-
-// get button when pressed will fetch and display a new cat image
+// get "replace kittie "button when clicked will fetch and display a new cat image
 const getReplaceKittieButton = buttons[0];
 
-// display new random image when clicking 'replace kittie' button
-getReplaceKittieButton.addEventListener("click", getCatImage);
+// display breed information when clicking 'replace kittie' button
+getReplaceKittieButton.addEventListener("click", getRandomImageOfBreed);
 
-// display the image on the page
+// get span element to display cat breed
+catBreedSpan = document.querySelector("#breedContainer span");
+catBreedDescription = document.querySelector("#breedContainer p:last-child");
 
-function displayCatImage(image) {
-  imageHolder.src = image;
+// function to display the fetched breed name
+function displayCatBreed(breed) {
+  catBreedSpan.innerHTML = breed;
 }
 
-// fetch random image from API
+// function to display the fetched description for this particular breed
+function displayCatDescription(description) {
+  catBreedDescription.innerHTML = description;
+}
 
-async function getCatImage() {
-  const response = await fetch("https://api.thecatapi.com/v1/images/search", {
+// display the image on the page
+function displayCatImage(isImage, breed, thisBreedsData) {
+  let breedImage;
+  if (isImage) {
+    breedImage = thisBreedsData.image.url;
+    imageHolder.src = breedImage;
+    displayCatBreed(breed);
+  } else {
+    imageHolder.src = "kittie.png";
+    displayCatBreed("No image available for this breed");
+  }
+}
+
+// api request constants
+const apiHeader = '["x-api-key"]';
+const key =
+  "live_XnQp1h9K9z3ObRCrpSLO8gAwOAPWG8nCMjMhvvgKcXHYV457i53UhGcTnufrwzl5";
+const breedsListApi = "https://api.thecatapi.com/v1/breeds";
+
+// fetch cat api image and description
+async function getRandomImageOfBreed() {
+  const response = await fetch(breedsListApi, {
     method: "GET",
     headers: {
-      ["x-api-key"]:
-        "live_XnQp1h9K9z3ObRCrpSLO8gAwOAPWG8nCMjMhvvgKcXHYV457i53UhGcTnufrwzl5",
+      apiHeader: key,
     },
   });
   const data = await response.json();
-  console.log(data);
-  const randomImage = await data[0].url;
-  displayCatImage(randomImage);
+  // count how many breeds have been returned
+  const breedCount = data.length;
+  // generat a random number within the size range of available breeds
+  const getRandomNumber = Math.floor(Math.random() * breedCount);
+  // select a random breed object from the available options
+  const getRandomBreed = data[getRandomNumber];
+  // get the breeds name
+  const breed = await getRandomBreed.name;
+  // get a description of the breed
+  const description = await getRandomBreed.description;
+  displayCatDescription(description);
+  // checks if there is an image for this breed
+  const isImage = "image" in getRandomBreed;
+  // if there is an image display it otherwise display default image
+  displayCatImage(isImage, breed, getRandomBreed);
 }
