0358fb0a052effaf837686917427c5c20724fc6d
ellis <lsp1@hotmail.com>
2022-09-05 21:50:12 -0600

    random image fetch and display on page

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+/* PAGE ELEMENTS */
+
+// get array of buttons on the page
+const buttons = document.querySelectorAll("button");
+
+// get the image element used to display the pages image and random images
+const imageHolder = document.querySelector('img[alt="No kittie for you!"]');
+
+/*  GET RANDOM FACT SCRIPTS */
+
 // get button when pressed will fetch and display a new cat fact
-const getFactButton = document.querySelector("button");
+const getFactButton = buttons[1];
 
 // display new cat fact when pressing 'Get fact' button
 getFactButton.addEventListener("click", getCatFact);
@@ -22,3 +32,33 @@ async function getCatFact() {
 
 // display fact on initial page visit
 getCatFact();
+
+/* GET RANDOM IMAGE SCRIPTS */
+
+// get button when pressed will fetch and display a new cat image
+const getReplaceKittieButton = buttons[0];
+
+// display new random image when clicking 'replace kittie' button
+getReplaceKittieButton.addEventListener("click", getCatImage);
+
+// display the image on the page
+
+function displayCatImage(image) {
+  imageHolder.src = image;
+}
+
+// fetch random image from API
+
+async function getCatImage() {
+  const response = await fetch("https://api.thecatapi.com/v1/images/search", {
+    method: "GET",
+    headers: {
+      ["x-api-key"]:
+        "live_XnQp1h9K9z3ObRCrpSLO8gAwOAPWG8nCMjMhvvgKcXHYV457i53UhGcTnufrwzl5",
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+  const randomImage = await data[0].url;
+  displayCatImage(randomImage);
+}
